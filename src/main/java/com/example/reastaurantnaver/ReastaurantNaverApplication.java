92a0c17366ae69e3484069d2b59dbb17271a6dec
jann <jjung5459@gmail.com>
2022-07-10 23:26:25 +0900

    Initial commit

---
+++ src/main/java/com/example/reastaurantnaver/ReastaurantNaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.reastaurantnaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReastaurantNaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReastaurantNaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reastaurantnaver/ReastaurantNaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reastaurantnaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReastaurantNaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
